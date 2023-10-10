3a919e31a0f1c02235b53e7bf88e6404ef4c5780
Alexander Rozum <roozumaleks@gmail.com>
2023-10-10 14:34:34 +0300

    Изучен Custom Builder, BindInstance. Использован custom builder

--- app/src/main/java/com/example/dependencyinjectionstart/example2/data/database/ExampleDatabase.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/data/database/ExampleDatabase.kt
@@ -6,11 +6,15 @@ import com.example.dependencyinjectionstart.R
 import javax.inject.Inject
 
 class ExampleDatabase @Inject constructor(
-    private val context: Context
+    private val context: Context,
+    private val currentTimeMillis: Long
 ) {
 
     fun method() {
-        Log.d(LOG_TAG, "ExampleDatabase:  ${context.getString(R.string.app_name)}")
+        Log.d(
+            LOG_TAG,
+            "ExampleDatabase:  ${context.getString(R.string.app_name)}, $currentTimeMillis"
+        )
     }
 
     companion object {
--- app/src/main/java/com/example/dependencyinjectionstart/example2/data/network/ExampleApiService.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/data/network/ExampleApiService.kt
@@ -6,11 +6,15 @@ import com.example.dependencyinjectionstart.R
 import javax.inject.Inject
 
 class ExampleApiService @Inject constructor(
-    private val context: Context
+    private val context: Context,
+    private val currentTimeMillis: Long
 ) {
 
     fun method() {
-        Log.d(LOG_TAG, "ExampleApiService  ${context.getString(R.string.app_name)}")
+        Log.d(
+            LOG_TAG,
+            "ExampleApiService  ${context.getString(R.string.app_name)}, $currentTimeMillis"
+        )
     }
 
     companion object {
