6a8539305cf7444b86b14f49b3782130b539ddc2
Alexander Rozum <roozumaleks@gmail.com>
2023-10-11 15:02:13 +0300

    Inject VM

--- app/src/main/java/com/example/dependencyinjectionstart/example2/di/ApplicationComponent.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/di/ApplicationComponent.kt
@@ -1,15 +1,12 @@
 package com.example.dependencyinjectionstart.example2.di
 
 import android.content.Context
-import com.example.dependencyinjectionstart.example2.data.database.ExampleDatabase
-import com.example.dependencyinjectionstart.example2.presentation.ExampleViewModel
 import com.example.dependencyinjectionstart.example2.presentation.MainActivity
 import dagger.BindsInstance
 import dagger.Component
-import javax.inject.Singleton
 
 @ApplicationScope
-@Component(modules = [DomainModule::class, DataModule::class])
+@Component(modules = [DomainModule::class, DataModule::class, ViewModelModel::class])
 interface ApplicationComponent {
 
     fun inject(activity: MainActivity)
