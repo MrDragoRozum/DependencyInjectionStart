e08b6f688c5f45cfd249ef8c3fb32bd03b09d8ef
Alexander Rozum <roozumaleks@gmail.com>
2023-10-11 15:30:46 +0300

    Использован Provider для VM в ViewModelFactory.kt

--- app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/ViewModelFactory.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/ViewModelFactory.kt
@@ -3,10 +3,11 @@ package com.example.dependencyinjectionstart.example2.presentation
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import javax.inject.Inject
+import javax.inject.Provider
 
 class ViewModelFactory @Inject constructor(
-    private val viewModels: @JvmSuppressWildcards Map<String, ViewModel>
+    private val viewModels: @JvmSuppressWildcards Map<String, Provider<ViewModel>>
 ) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>) =
-        viewModels[modelClass.simpleName] as T
+        viewModels[modelClass.simpleName]?.get() as T
 }
